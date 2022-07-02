1b200e44cb0edd46cd3001188508459170f992f5
JuanPerez <jkml563@gmail.com>
2022-07-01 19:12:13 -0500

    Aplicando estilos css a Angular

--- mi-primera-app/src/app/personas/personas.component.ts
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -2,8 +2,13 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-personas',
-  templateUrl: './personas.component.html'
+  templateUrl: './personas.component.html',
+  //styleUrls: ['./personas.component.css']
+  styles:[`
+  h1{
+    color:blue;
+  }`]
 })
 export class PersonasComponent{
-  
+
 }
