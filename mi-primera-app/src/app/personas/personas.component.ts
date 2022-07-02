0bb9ad21631c2c9d039abee1bf3b770edf81d4a2
JuanPerez <jkml563@gmail.com>
2022-07-02 12:30:15 -0500

    event Binding en Angular

--- mi-primera-app/src/app/personas/personas.component.ts
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -3,12 +3,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
-  //styleUrls: ['./personas.component.css']
-  styles:[`
-  h1{
-    color:blue;
-  }`]
+  styleUrls: ['./personas.component.css']
+
 })
 export class PersonasComponent{
 desabilitar = false;
+mensaje = 'No se ha agregado ninguna persona.';
+
+agregarPersona(){
+this.mensaje = 'Persona Agregada';
+}
 }
